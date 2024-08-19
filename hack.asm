c60afd42e3147f7c5bc3d5853a6c3f081030a077
lightbulbsun <lightbulbsun.dev@gmail.com>
2024-08-19 15:23:49 +0200

    fix player 2
    Fixes #1

--- hack.asm
+++ hack.asm
@@ -1,60 +1,10 @@
-MASK_BUTTON_A   = $40
 MASK_BUTTON_C   = $20
 NUM_WEAPONS     = 3
-CUR_WEAPON      = $fff679
+WEAPON_P1       = $fff679
+WEAPON_P2       = $fff6b9
 
     org 0
     incbin "batman.md"
-
-; only A-presses fire weapon
-    org $04ae2c
-            dw      MASK_BUTTON_A
-    org $04af14
-            dw      MASK_BUTTON_A
-    org $04b004
-            dw      MASK_BUTTON_A
-    org $04b208
-            dw      MASK_BUTTON_A
-    org $04b318
-            dw      MASK_BUTTON_A
-    org $04b3a2
-            dw      MASK_BUTTON_A
-    org $04b420
-            dw      MASK_BUTTON_A
-    org $04b4b2
-            dw      MASK_BUTTON_A
-    org $04b538
-            dw      MASK_BUTTON_A
-    org $04b69e
-            dw      MASK_BUTTON_A
-    org $04b756
-            dw      MASK_BUTTON_A
-    org $04d052
-            dw      MASK_BUTTON_A
-    org $04d968
-            dw      MASK_BUTTON_A
-    org $04da50
-            dw      MASK_BUTTON_A
-    org $04db40
-            dw      MASK_BUTTON_A
-    org $04dd44
-            dw      MASK_BUTTON_A
-    org $04de54
-            dw      MASK_BUTTON_A
-    org $04dede
-            dw      MASK_BUTTON_A
-    org $04df5c
-            dw      MASK_BUTTON_A
-    org $04dfee
-            dw      MASK_BUTTON_A
-    org $04e074
-            dw      MASK_BUTTON_A
-    org $04e1da
-            dw      MASK_BUTTON_A
-    org $04e292
-            dw      MASK_BUTTON_A
-    org $04fb8e
-            dw      MASK_BUTTON_A
 
 ; picking up disks never changes weapon
     org $013d0e
@@ -65,23 +15,60 @@ CUR_WEAPON      = $fff679
     org $013cf6
             bra     $13d06
 
-; C-presses cycle through all weapons
+    org $002032
+            jmp     suppress_c_presses
+
     org $0087c2
-            jsr     my_function
+            jsr     player1
+
+    org $0087ca
+            jsr     player2
+
     org $1ff000
-my_function:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+suppress_c_presses:
+            and.w   (a2)+, d0
+            and.w   #$dfdf, d0
+            beq.w   .do_nothing
+.do_something
+            jmp     $2d22
+.do_nothing
+            jmp     $2038
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+player1:
 .check_for_new_c_press
             move.b  ($22, a4), d0
             and.b   #MASK_BUTTON_C, d0
             beq     .the_end
 .cont
-            move.b  (CUR_WEAPON), d0
+            move.b  (WEAPON_P1), d0
             add.b   #1, d0
             cmp.b   #NUM_WEAPONS, d0
             bne     .change_weapon
             move.b  #0, d0
 .change_weapon
-            move.b  d0, (CUR_WEAPON)
+            move.b  d0, (WEAPON_P1)
+.the_end
+            ; replace original instructions
+            movea.w (6, a6), a6
+            movea.l (a6), a5
+            rts
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+player2:
+.check_for_new_c_press
+            move.b  ($22, a4), d0
+            and.b   #MASK_BUTTON_C, d0
+            beq     .the_end
+.cont
+            move.b  (WEAPON_P2), d0
+            add.b   #1, d0
+            cmp.b   #NUM_WEAPONS, d0
+            bne     .change_weapon
+            move.b  #0, d0
+.change_weapon
+            move.b  d0, (WEAPON_P2)
 .the_end
             ; replace original instructions
             movea.w (6, a6), a6
